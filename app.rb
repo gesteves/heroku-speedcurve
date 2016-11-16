5e1f5faedfdefa5dcc17484a9825ab628681ffde
Jack Chu <jack@jackchu.com>
2016-11-16 16:55:54 -0500

    Upgrade to ruby 2.3.1

--- app.rb
+++ app.rb
@@ -5,14 +5,14 @@ require "httparty"
 post "/deploy/:key" do
   url = "https://api.speedcurve.com/v1/deploy"
   body = {
-    'detail' => "#{params[:app]} #{params[:release]} was deployed to Heroku by #{params[:user]}",
-    'note' => "#{params[:app]} #{params[:release]}"
+    detail: "#{params[:app]} #{params[:release]} was deployed to Heroku by #{params[:user]}",
+    note: "#{params[:app]} #{params[:release]}",
   }
   auth = {
-    :username => params[:key],
-    :password => 'x'
+    username: params[:key],
+    password: "x",
   }
-  response = HTTParty.post(url, :body => body, :basic_auth => auth)
+  response = HTTParty.post(url, body: body, basic_auth: auth)
   status response.code
   body response.body
 end
